0de92cd13d181a2563a66ccb815b87832bc10fcf
CodyAustinDavis <CodyAustinDavis>
2022-08-21 23:31:45 +0000

    Remove streaming demos

--- IoT Data Streaming Demos/DeltaLiveTablesIoTPipeline/TestStreamFromSilverTarget.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-
-# COMMAND ----------
-
-df = spark.readStream.table("codydltdemos.silver_all_sensors")
--- IoT Data Streaming Demos/Get Global View.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC 
-# MAGIC SELECT 
-# MAGIC *
-# MAGIC FROM global_temp.unique_sensors
